0cc81c1d9953b2e507adc64ee67eb7a26d06809b
AbdallaEl <101177973+AbdallaEl@users.noreply.github.com>
2026-07-15 11:39:04 +0300

    Update rewayatclub.ts

--- plugins/arabic/rewayatclub.ts
+++ plugins/arabic/rewayatclub.ts
@@ -7,7 +7,7 @@ import { defaultCover } from '@libs/defaultCover';
 class RewayatClub implements Plugin.PagePlugin {
   id = 'rewayatclub';
   name = 'Rewayat Club';
-  version = '1.0.1';
+  version = '1.0.2';
   icon = 'src/ar/rewayatclub/icon.png';
   site = 'https://rewayat.club/';
 
